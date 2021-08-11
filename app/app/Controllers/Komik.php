4679f96dc2ef597c183f3300bacfc825726ff09e
firmanarrozi <47202151+firmanarrozi@users.noreply.github.com>
2021-08-11 19:43:58 +0700

    Revisi1

---
+++ app/app/Controllers/Komik.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+use Config\Validation;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+    public function __construct()
+    {
+        $this->komikModel = new KomikModel();
+    }
+    
+    public function index()
+    {
+        // $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik',
+            'komik' => $this->komikModel->getKomik()
+        ];
+        
+        // $komikModel = new KomikModel();
+
+
+        return view('komik/index', $data);
+    } 
+
+    public function detail($slug)
+    {
+        $komik = $this->komikModel->getKomik($slug);
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug) 
+        ];
+
+        // Jika komik tidak ada di tabel
+        if(empty($data['komik']))
+        {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik ' .$slug. ' tidak ditemukan');
+        }
+
+        return view('komik/detail', $data);
+    }
+
+    public function create()
+    {
+        // session();
+
+        $data = [
+            'title' => 'Form Tambah Data Komik',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('komik/create', $data);
+    }
+
+    public function save()
+    {
+        // validasi input
+        if(!$this->validate([
+            'judul' => 'required|is_unique[komik.judul]'
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('\komik\create')->withInput()->with('validation', $validation);
+        }
+
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+
+        $this->komikModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+        
+        session()->setFlashdata('pesan','Data berhasil ditambahkan');
+
+        return redirect()->to('/komik');
+    }
+<<<<<<< HEAD:app/Controllers/Komik.php
+
+    public function delete($id)
+    {
+        $this->komikModel->delete($id);
+        session()->setFlashdata('pesan','Data berhasil dihapus');
+        return redirect()->to('/komik');
+    }
+
+    public function edit($slug)
+    {
+        $data = [
+            'title' => 'Form Ubah Data Komik',
+            'validation' => \Config\Services::validation(),
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+
+        return view('komik/edit', $data); 
+    }
+
+    public function update($id)
+    {
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+
+        $this->komikModel->save([
+            'id' => $id,
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+        
+        session()->setFlashdata('pesan','Data berhasil diubah');
+
+        return redirect()->to('/komik');
+    }
+
+
+}
+=======
+}
+>>>>>>> be0a69f4ed835765e0ead76200e1ab25d58b093b:app/Controllers/Mahasiswa.php
